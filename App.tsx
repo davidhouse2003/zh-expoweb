39410644d705fa23105ef65d3730a087d4b99471
davidhouse2003 <davidhouse2003@gmail.com>
2021-02-13 15:24:37 -0700

    works

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    console.log("inside App.useEffect()")
+   
+}, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>00 Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
