9b40816a10eadd8d3a6ba7baf310f013d007839c
davidhouse2003 <davidhouse2003@gmail.com>
2021-02-14 09:39:17 -0700

    works

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ export default function App() {
 }, []);
   return (
     <View style={styles.container}>
-      <Text>02 zh-expoweb</Text>
+      <Text>03 zh-expoweb</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
